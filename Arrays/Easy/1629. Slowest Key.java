d5c89383b4108979289502fcd1179aef0ba23b1a
qianlang95 <74698914+qianlang95@users.noreply.github.com>
2021-01-28 20:19:29 -0800

    Create 1629. Slowest Key.java

---
+++ Arrays/Easy/1629. Slowest Key.java
@@ -0,0 +1,31 @@
+//calculate distance and record the longest
+/*
+A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+
+You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+
+The tester wants to know the key of the keypress that had the longest duration. The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+
+Note that the same key could have been pressed multiple times during the test, and these multiple presses of the same key may not have had the same duration.
+
+Return the key of the keypress that had the longest duration. If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+*/
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        if (releaseTimes.length==0) return ' ';
+        char key=keysPressed.charAt(0);
+        int duration=releaseTimes[0];
+        for (int i=1; i<keysPressed.length(); i++){
+            char cur_key=keysPressed.charAt(i);
+            if (releaseTimes[i]-releaseTimes[i-1]>duration){
+                key=cur_key;
+                duration=releaseTimes[i]-releaseTimes[i-1];
+            }else if (releaseTimes[i]-releaseTimes[i-1]==duration && cur_key>key){
+                key=cur_key;
+                duration=releaseTimes[i]-releaseTimes[i-1];
+            }
+        }
+        return key;
+        
+    }
+}
