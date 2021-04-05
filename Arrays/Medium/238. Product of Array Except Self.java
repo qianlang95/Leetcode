254980faeb44718241c8049ae89f1fd2275cd79c
qianlang95 <74698914+qianlang95@users.noreply.github.com>
2021-04-04 22:28:19 -0700

    Update 238. Product of Array Except Self.java

--- Arrays/Medium/238. Product of Array Except Self.java
+++ Arrays/Medium/238. Product of Array Except Self.java
@@ -1,1 +1,22 @@
+//Use two passes, left to right and right to left. use result array to remember the results
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+*/
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int res[]= new int[nums.length];
+        res[0]=nums[0];
+        for (int i=1; i<nums.length; i++){
+            res[i]=nums[i]*res[i-1];
+        }
+        int left=1;
+        for(int i=nums.length-1; i>0; i--){
+            res[i]=left*res[i-1];
+            left*=nums[i];
+        }
+        res[0]=left;
+        return res;
+    }
+}
